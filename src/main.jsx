5e5214ade7a0efe1343b6880a5e919bfb9b1cb89
Yash5704 <yashpatel07052004@gmail.com>
2025-08-02 11:26:02 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './style files/chapters.css'
-import './style files/videos.css'
-import './style files/about.css'
-import './style files/help.css'
-import './style files/contact.css'
-import './style files/loginpage.css'
-import './style files/index.css'
+import './style-files/chapters.css'
+import './style-files/videos.css'
+import './style-files/about.css'
+import './style-files/help.css'
+import './style-files/contact.css'
+import './style-files/loginpage.css'
+import './style-files/index.css'
 
 createRoot(document.getElementById('root')).render(
     <App />
